d940bff553b8c99bd6124660b8c15ac762f89c1c
mousumitirumo <48582707+mousumitirumo@users.noreply.github.com>
2019-03-17 00:15:47 +0530

    fn,ln functions practice

--- first-script.R
+++ first-script.R
@@ -6,4 +6,8 @@ B = c(8, 13, 21)
 # matrix 
 
 mat = (c(6,7,8,9), ncol = 2)
-
+ fn = 'Mousumi'
+ ln = 'Chakraborty'
+ ins = 'ANDC'
+ course = 'Bsc(H) Biomedical Science'
+ paste(sep = '',fn,' ',ln,',',' ',course,',',' ',ins,'.')
